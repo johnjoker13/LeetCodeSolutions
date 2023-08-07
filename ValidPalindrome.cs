3572c12f69077da88b00e1ff874a665b05231f1e
joaoOliveira <joaoavelino2008@hotmail.com>
2023-08-07 17:05:07 -0300

    ValidPalidrome

---
+++ ValidPalindrome.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int l = 0;
+        int r = s.Length - 1;
+        while (l <= r) {
+          char currLeft = s[l];
+          char currRight = s[r];
+
+          if (!Char.IsLetterOrDigit(currLeft)) {
+              l++;
+          } else if (!Char.IsLetterOrDigit(currRight)) {
+              r--;
+          } else {
+              if (Char.ToLower(currLeft) != Char.ToLower(currRight)) {
+                  return false;
+              }
+              l++;
+              r--;
+          }
+        }
+
+        return true;
+    }
+}
